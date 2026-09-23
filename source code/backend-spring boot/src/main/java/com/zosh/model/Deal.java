068c4d9b6af8243c1ead926377075a6348baec3c
Kritiman Singh <kritimansingh123@gmail.com>
2026-09-23 22:54:03 +0530

    Fix MySQL prod startup: use IDENTITY instead of AUTO for ID generation

--- source code/backend-spring boot/src/main/java/com/zosh/model/Deal.java
+++ source code/backend-spring boot/src/main/java/com/zosh/model/Deal.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Deal {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer discount;
 
